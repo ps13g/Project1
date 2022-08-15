14d41b7f0c369c1967876142a91f5f20c44775a0
ps13g <saketh.2004@gmail.com>
2022-08-15 12:45:53 -0600

    Hello World new line

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,7 +1,9 @@
 public class helloWorld {
     public static void main(String args[]){
         int myAge = 100;
+        int temperature;
 
+        temperature = 95
         System.out.println("Hello\nWorld");
         System.out.print("Wow I dont care");
         System.out.println("Lets see if this runs ");
