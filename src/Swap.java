8cc4201865398dc028ded304bee7b832c9cc6b33
ps13g <saketh.2004@gmail.com>
2022-08-17 12:12:17 -0600

    Swap file

--- src/Swap.java
+++ src/Swap.java
@@ -2,6 +2,7 @@ public class Swap {
     public static void main(String args[]) {
 
         int numA = 4, numB = 9;
-        System.out.println();
+        System.out.println("First line: " + numA + "\nSecond number: " + numB);
+
     }
 }
